152ec679a0c9af4da32d9719241eef07b44ed552
mohsin324 <mohsinmunir324@gmail.com>
2023-08-03 18:09:42 +0500

    learning javascript array, array method, higher order array method, string, string method

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log(`--------Learning Higher Order Array Methods---------`);
+///////////////////////////////////////////////      array 
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log('before sorting');
+// console.log(cars);
+// console.log('after sorting');
+// console.log(cars.sort())
+
+// const alphabets = ['x', 'a', 'c', ' b', 'n']
+// console.log('before sorting');
+// console.log(alphabets.toString().trim());
+// console.log('after sorting');
+// console.log(alphabets.sort());
+
+// const cars = new Array("Saab", "Volvo", "BMW");
+// console.log(cars)
+// cars[0] = "MG"
+// console.log(cars.toString())
+
+// const person = ["John", "Doe", 69];
+// console.log(person.toString())
+
+
+// const point = [40];
+// console.log(point)
+
+// ///////////////////////////////////////////  array method
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Watermelon", "Apricot"];
+console.log(fruits);
+console.log(`-----------------------Before any Method-----------------------`);
+// ******************* length
+// console.log(fruits.length);
+
+// ******************* string
+// console.log(fruits.toString());
+// let str = fruits.toString()
+// console.log(str.slice(0, 5)+ ' str slice ')
+
+// ********************  join
+// ***** it return string but with join function *
+// let join = fruits.join("*");
+// console.log(join)
+
+// ********************  popping and push
+// **** pop remove the last element
+// console.log(fruits.pop());       return the pop element 
+// fruits.pop();
+// console.log(fruits,  ' pop element')
+// ***** push add element in the last of array
+// fruits.push("Nuts");
+// console.log(fruits+ ' push element ')
+
+
+// ********************  shifting element
+// **** shift will remove first element from array
+// fruits.shift()
+// console.log(fruits)
+// 
+
